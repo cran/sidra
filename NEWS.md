c7a25a0ced521324c339cccf7e7df64c7b1d3a3a
Rodrigo Emmanuel Santana Borges <rodrigo@borges.net.br>
2025-08-27 12:00:09 +0000

    version 0.1.7

---
+++ NEWS.md
@@ -0,0 +1,32 @@
+# sidra 0.1.7
+
+* Added helper function and api url check handling with graceful exit if no access
+
+# sidra 0.1.6
+
+* Fixes documentation of functions and DESCRIPTION for resubmitting to CRAN
+
+# sidra 0.1.5
+
+* Fixes on sidra function return and optimization for reducing build and test 
+times for CRAN
+
+# sidra 0.1.4
+
+* Fixes for CRAN and added tab_agrs function
+
+# sidra 0.1.3
+
+* Initial CRAN submission.
+
+# sidra 0.1.2
+
+* Added tab_search function
+
+# sidra 0.1.1
+
+* first preliminary main sidra function
+
+# sidra 0.1.0
+
+* Initial version
---
+++ tests/testthat/test-sidra.R
@@ -0,0 +1,16 @@
+# context("checa-saida")  # Script "test-checa-saida.R"
+library(testthat)        # load testthat package
+library(sidra)       # load our package
+
+
+# Test whether the output is a data frame
+test_that("sidra(1705) returns a data frame", {
+  output_table <- sidra(1705)
+  if(is.null(call_ibge({httr::GET('https://servicodados.ibge.gov.br/api/v3/agregados',config = httr::timeout(2))}))) {
+    expect_null(output_table,info = "API was not reacheable,
+                function should have returned NULL")
+
+  } else {
+  expect_type(output_table, "data.frame")
+  succeed("API was reachable and a data.frame was returned.")
+}})
